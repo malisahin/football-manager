45d10fe2f550dc80fda00b88e664dabbeb38fe1b
Sahinogullari <mehmetalisahinogullari@hotmail.com>
2019-06-21 01:33:05 +0300

    Initial commit

---
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.example.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+ * author: mali.sahin
+ * since: 21.06.2019
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
