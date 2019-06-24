da00b30c5bac878821611aebc8bdb1252cc408c0
ali.sahinogullari <mehmetalisahinogullari@gmail.com>
2019-06-24 09:23:08 +0300

    Shared Library Dependency

---
+++ shared-library/src/main/java/com/works/sharedlibrary/service/AbstractService.java
@@ -0,0 +1,15 @@
+package com.works.sharedlibrary.service;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * @author mali.sahin
+ * @since 6/24/19.
+ */
+public abstract class AbstractService {
+
+  protected final Logger logger = LogManager.getLogger(this.getClass());
+
+
+}
