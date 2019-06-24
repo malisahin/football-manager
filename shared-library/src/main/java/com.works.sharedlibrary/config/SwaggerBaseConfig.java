6addcdd7fbb0bf9143d896f48d9574d06cbc48c7
ali.sahinogullari <mehmetalisahinogullari@gmail.com>
2019-06-24 08:56:41 +0300

    Swagger Implementation

---
+++ shared-library/src/main/java/com.works.sharedlibrary/config/SwaggerBaseConfig.java
@@ -0,0 +1,22 @@
+package com.works.sharedlibrary.config;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author mali.sahin
+ * @since 6/24/19.
+ */
+
+public abstract class SwaggerBaseConfig {
+
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
+  }
+}
